bc84a05cc4c56ccc46fd9aa9f5b0bf5e7bfab600
xxxyouthxxx <1042151752@qq.com>
2023-02-28 14:01:07 +0800

    again

--- src/views/Canvas.vue
+++ src/views/Canvas.vue
@@ -1,6 +1,6 @@
 <template>
-	<div class="canvas-container" ref="container" @mousedown="handleMouseDown" @mouseup="handleMouseUp" @mousemove="handleMouseMove" @wheel="handleWheel">
-	  <canvas ref="canvas" :width="canvasWidth" :height="canvasHeight" style="background-color: white;"></canvas>
+	<div>
+	  <canvas ref="canvas" :width="canvasWidth" :height="canvasHeight" @click="handleClick"></canvas>
 	</div>
   </template>
   
@@ -8,85 +8,48 @@
   export default {
 	data() {
 	  return {
-		canvasWidth: 600, // Canvas宽度
-		canvasHeight: 400, // Canvas高度
-		scale: 1, // Canvas缩放比例
-		dragging: false, // 是否正在拖动
-		lastX: 0, // 上一个点的X坐标
-		lastY: 0 // 上一个点的Y坐标
-	  };
+		canvasWidth: 1000,
+		canvasHeight: 1000,
+		pixels: Array.from({ length: 50 }, () => Array.from({ length: 50 }, () => ({ color: '#ffffff' })))
+	  }
 	},
 	mounted() {
-	  // 获取Canvas和Canvas容器
-	  const canvas = this.$refs.canvas;
-	  const container = this.$refs.container;
-  
-	  // 设置初始Canvas位置
-	  canvas.style.transform = `translate(${container.offsetWidth / 2 - this.canvasWidth / 2}px, ${container.offsetHeight / 2 - this.canvasHeight / 2}px)`;
-  
-	  // 监听窗口大小变化
-	  window.addEventListener('resize', () => {
-		canvas.style.transform = `translate(${container.offsetWidth / 2 - this.canvasWidth / 2}px, ${container.offsetHeight / 2 - this.canvasHeight / 2}px)`;
-	  });
+	  const canvas = this.$refs.canvas
+	  const ctx = canvas.getContext('2d')
+	  this.renderCanvas(ctx)
 	},
 	methods: {
-	  handleMouseDown(event) {
-		// 鼠标按下开始拖动
-		this.dragging = true;
-		this.lastX = event.clientX;
-		this.lastY = event.clientY;
-	  },
-	  handleMouseUp() {
-		// 鼠标松开停止拖动
-		this.dragging = false;
-	  },
-	  handleMouseMove(event) {
-		if (this.dragging) {
-		  // 计算鼠标移动距离
-		  const deltaX = event.clientX - this.lastX;
-		  const deltaY = event.clientY - this.lastY;
-  
-		  // 更新Canvas位置
-		  const canvas = this.$refs.canvas;
-		  const transform = window.getComputedStyle(canvas).getPropertyValue('transform');
-		  const matrix = new DOMMatrix(transform);
-		  canvas.style.transform = `matrix(${matrix.a}, ${matrix.b}, ${matrix.c}, ${matrix.d}, ${matrix.e + deltaX}, ${matrix.f + deltaY})`;
-  
-		  // 保存当前鼠标位置
-		  this.lastX = event.clientX;
-		  this.lastY = event.clientY;
+	  renderCanvas(ctx) {
+		const pixelSize = 20
+		for (let i = 0; i < this.pixels.length; i++) {
+		  for (let j = 0; j < this.pixels[i].length; j++) {
+			const { color } = this.pixels[i][j]
+			ctx.fillStyle = color
+			ctx.fillRect(i * pixelSize, j * pixelSize, pixelSize, pixelSize)
+		  }
 		}
+		console.log(this.pixels);
 	  },
-	  handleWheel(event) {
-		// 阻止页面滚动
-		event.preventDefault();
-  
-		// 计算缩放比例
-		const delta = Math.sign(event.deltaY);
-		const scaleDelta = delta * 0.1;
-		this.scale = Math.max(0.1, Math.min(10, this.scale + scaleDelta));
-  
-		// 更新Canvas缩放
-		const canvas = this.$refs.canvas;
-		canvas.style.transform = `scale(${this.scale})`;
+	  handleClick(e) {
+		const canvas = this.$refs.canvas
+		const pixelSize = 20
+		const rect = canvas.getBoundingClientRect()
+		const x = Math.floor((e.clientX - rect.left) / pixelSize)
+		const y = Math.floor((e.clientY - rect.top) / pixelSize)
+		const color = '#000000' // 用户选择的颜色，可以通过其他方式获取
+		this.pixels[x][y].color = color
+	  }
+	},
+	watch: {
+	  pixels: {
+		handler() {
+		  const canvas = this.$refs.canvas
+		  const ctx = canvas.getContext('2d')
+		  this.renderCanvas(ctx)
+		},
+		deep: true
 	  }
 	}
-  };
+  }
   </script>
-  
-  <style scoped>
-  .canvas-container {
-	width: 100%;
-	height: 100%;
-	position: absolute;
-	top: 0;
-	left: 0;
-	background-color: #333;
-	overflow: hidden;
-	user-select: none;
-  }
-  canvas {
-	border: 1px solid black;
-  }
-  </style>
   
